64bbe44b63617fbf5a0e2c66354d0d8d0d200619
User <bipu799@gmail.com>
2018-10-20 10:58:10 +0600

    1.Added firebase database and authenticationl
    2.Added Login Registration page for Driver and customer
    
    3.Added MapActivities for Diver and Customer using googlemaps API.

---
+++ app/src/main/java/com/example/user/uberclone/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.user.uberclone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.user.uberclone.auth.CustomerLoginActivity;
+import com.example.user.uberclone.auth.DriverLoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mDriver, mCustomer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDriver = findViewById(R.id.driver);
+        mCustomer = findViewById(R.id.customer);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
